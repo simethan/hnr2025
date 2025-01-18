7d81783c59aee3952c029217e1f13559990b7b40
Qin Guan <qinguan20040914@gmail.com>
2025-01-18 18:03:54 +0800

    new file

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,7 +6,6 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { CookedMeter } from '@/app/components/CookedMeter'
 import { supabase } from '@/lib/supabase'
-import { BrightDataResponse } from '@/lib/brightdata'
 
 export default function Profile() {
   const [linkedinUrl, setLinkedinUrl] = useState('')
@@ -18,50 +17,6 @@ export default function Profile() {
   useEffect(() => {
     fetchProfile()
   }, [])
-
-  async function getRating() {
-    'use server'
-
-    // GitHub stuff
-
-    // Resume stuff
-
-    // LinkedIn stuff
-    const key = process.env.BRIGHTDATA_KEY
-    const data: { snapshot_id: string } = await (await fetch("https://api.brightdata.com/datasets/v3/trigger?dataset_id=gd_l1viktl72bvl7bjuj0&include_errors=true", {
-      body: JSON.stringify([
-        {
-          url: linkedinUrl
-        }
-      ]),
-      method: "POST",
-      headers: {
-        Authorization: 'Bearer ' + key,
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include'
-    })).json()
-
-    for (let i = 0; i < 100; i++) {
-      const res: BrightDataResponse | { status: 'running' } = await (await fetch(`https://api.brightdata.com/datasets/v3/snapshot/${data.snapshot_id}?format=json`, {
-        headers: {
-          Authorization: 'Bearer ' + key,
-        },
-        credentials: 'include'
-      })).json()
-
-      if ('status' in res) {
-        await Promise.resolve(r => setTimeout(r, 3000))
-        continue 
-      }
-
-      const profile = res[0]
-
-      console.log(profile)
-      
-      // Send to gpt
-    }
-  }
 
   const fetchProfile = async () => {
     const { data: { user } } = await supabase.auth.getUser()
